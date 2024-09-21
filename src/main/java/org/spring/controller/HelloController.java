7532d45407c7da3fddedd5722c6635dc48fd4976
yuanzx <yuanzx1515@gmail.com>
2024-09-22 04:33:19 +0800

    模板1

--- src/main/java/org/spring/controller/HelloController.java
+++ src/main/java/org/spring/controller/HelloController.java
@@ -1,14 +1,24 @@
 package org.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@RequestMapping("/test")
 @Controller
 public class HelloController {
 
-    @RequestMapping("/")
-    public ModelAndView hello(){
+    @RequestMapping("/a")
+    public ModelAndView hello(Model model) {
+        model.addAttribute("name", "Hello World");
       return new ModelAndView("index");
+    }
+
+
+    @RequestMapping("/b")
+    public ModelAndView test(Model model) {
+        model.addAttribute("name", "yzx");
+        return new ModelAndView("index");
     }
 }
